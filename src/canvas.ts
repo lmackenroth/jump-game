9ed258643c473a8b9591cbbf69e5c670eec5f126
lmackenroth <lmackenroth@smu.edu>
2024-12-01 11:12:10 -0600

    we've got platforms! they are not interactable yet

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,21 +1,28 @@
 import P5Lib from 'p5';
 
 import { Rectangle } from './rectanglecontrols';
+import { platform } from './platforms';
 
 function project(p5: P5Lib) {
     let rect: Rectangle;
+    let plat: platform;
 
     p5.setup = () => {
         p5.createCanvas(500, 500);
         rect = new Rectangle(p5.width / 2 - 25, p5.height - 50, 50, 50, 10);
+        plat = new platform(p5);
+        plat.createPlatfroms(5);
     };
 
     p5.draw = () => {
         p5.background(20, 70, 100); // Clear canvas every frame
-
+        //create platforms then draw them
+        plat.draw();
         // Move and draw the rectangle
         rect.move(p5);
         rect.draw(p5);
+
+        
     };
 
     p5.keyPressed = () => {
--- src/platforms.ts
+++ src/platforms.ts
@@ -4,6 +4,7 @@ export class platform {
 
     public p5: P5Lib;
     //array of rectangles, maybe pass number or rectangles to its method
+    //stores each rectangles starting position, its width, and height
     public platforms: Array<{ x: number; y: number; width: number; height: number }>;
 
     constructor(p5: P5Lib) {
@@ -13,7 +14,13 @@ export class platform {
 
     //I want to spawn in rectangles at a certain length but in random positions
     draw() {
-        //draw rectangles
+        //will change color later
+        this.p5.fill(255); 
+        //draw rectangles with stored values
+        for (const platform of this.platforms) {
+            this.p5.rect(platform.x, platform.y, platform.width, platform.height);
+        }
+        console.log("drawing platforms");
 
 
     }
@@ -21,23 +28,19 @@ export class platform {
     createPlatfroms(amount: number) {
         //fill array with a cdertain number of platforms
 
-        for (var i = 0; i < amount; i++) {
+        for (var i = 0; i <= amount; i++) {
             //very vauge constrictions on the randomness, will have to tweak later
             const platform = {
                 x: this.p5.random(0, this.p5.width), //x position
                 y: this.p5.random(0, this.p5.height), //y position
                 width: this.p5.random(40, 80), // width 
                 height: this.p5.random(10, 30), // height
+                
             };
+            console.log("creating platform");
+            //add to array
             this.platforms.push(platform);
 
-
-
-
-
         }
-
-
-
     }
 }
