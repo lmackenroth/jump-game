1b77b0c168d6b0b3e225657fc6837c09a4306fc2
lmackenroth <lmackenroth@smu.edu>
2024-11-30 21:51:35 -0600

    Rectangle jumps! but i need to delete the trailing squares

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,27 +1,55 @@
 import P5Lib from 'p5';
-//let movingUp: boolean;
+let movingUp: boolean;
 let rectY: number;
+let rectX: number;
+let moveSpeed: number = 5;
+
 //project set up
 
 function project(p5: P5Lib) {
 
     p5.setup = () => {
-        p5.createCanvas(500,500);
-        p5.background(20,70,100);
+        p5.createCanvas(500, 500);
+        p5.background(20, 70, 100);
         //set size of rectangle
         rectY = p5.height - 50;
-        //checks when the rectangle is moving up
-        //movingUp = false;
+        rectX = p5.width - 50;
+        //cset to false when loaded in
+        movingUp = false;
 
     }
 
     p5.draw = () => {
-        p5.rect(p5.width / 2 - 25, rectY, 50, 50);
- 
+        //create rectangle and spawn point based on canvas
+        p5.rect(rectX / 2 - 25, rectY, 50, 50);
+
+        if (movingUp) {
+
+            rectY -= moveSpeed;
+            if (rectY <= 100) {
+
+                movingUp = false;
+            } 
+
+
+        }else if (rectY < p5.height - 50) {
+
+            rectY += moveSpeed;
+
+        }
+
     }
     p5.keyPressed = () => {
+        // Check if the spacebar is pressed
+        if (p5.key === 'a') { 
+            // Only move up if the rectangle is at the bottom
+            if (rectY >= p5.height - 50) { 
+                movingUp = true;
+                console.log("a is pressed");
+    
+            }
 
-
+        }
     }
 
 }
